de7f6602778276cb1d53f6b11c42fa407f7a6771
Samolazov Roman <samolazov.roman@gmail.com>
2017-09-04 13:01:47 +0300

    Update install.sh
    change version terraform and packer

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 
-TERRAFORM_VERSION="0.9.6"
-PACKER_VERSION="1.0.0"
+TERRAFORM_VERSION="0.10.3"
+PACKER_VERSION="1.0.4"
 # create new ssh key
 [[ ! -f /home/ubuntu/.ssh/id_rsa ]] \
 && mkdir -p /home/ubuntu/.ssh \
